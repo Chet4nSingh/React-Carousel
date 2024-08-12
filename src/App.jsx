1a5a18cb5d9a837c224f84f1f2d32e7b394143f7
Chetan Singh <chet4nsingh@gmail.com>
2024-08-12 07:38:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
 function App() {
-  return <h1 className="text-9xl">Carousel</h1>
+  return (
+    <>
+      <h1 className="text-9xl">Carousel</h1>
+      <main>
+        <div id="carousel-container">
+          <div id="slides">
+            <div id="slide-one"></div>
+            <div id="slide-two"></div>
+            <div id="slide-three"></div>
+          </div>
+          <div id="btns">
+            <button id="prev"></button>
+            <button id="next"></button>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default App;
